009ff1e07307375fd76702c9b466dbd9fd0100fa
KeerMcR <119884975+KeerMcR@users.noreply.github.com>
2024-11-16 19:15:12 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,6 +1,6 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 count = 0
-while count <= len(my_list):
+while count < len(my_list):
     if my_list[count] < 0:
         break
     elif my_list[count] > 0:
